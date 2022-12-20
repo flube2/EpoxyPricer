cd8920d580c218846c44ee1630aa55f8510a7ebf
Frank Lubek <flubek@gmail.com>
2022-12-20 10:11:11 -0600

    formatting

--- src/EpoxyProductPackage/Main.java
+++ src/EpoxyProductPackage/Main.java
@@ -1,45 +1,12 @@
-package EpoxyProductPackage; 
-
+package EpoxyProductPackage;
 
 public class Main {
 
-	
 	public static void main(String[] args) {
-	// Main programming logic goes here
-	EpoxyProduct ep = new EpoxyProduct(2.0, SIZE.SMALL, BEL.NONE, AEL.NONE, GLOW.NONE);
-	ep.printEpoxyProductInfo();
-	return;
-	
+		// Main programming logic goes here
+		EpoxyProduct ep = new EpoxyProduct(2.0, SIZE.SMALL, BEL.NONE, AEL.NONE, GLOW.NONE);
+		ep.printEpoxyProductInfo();
+		return;
 	}
-	/*class EpoxyProduct {
-		
-	public double weight; // in fluid ounces (fl oz)
-	
-		
-	public enum Size {
-	    TINY, SMALL, MEDIUM, LARGE, HUGE;
-	}
-	
-	public enum BEL {
-		HARDLY, LITTLE, MEDIUM, LOTS, TONS;
-	}
-	
-	public enum AEL {
-		HARDLY, LITTLE, MEDIUM, LOTS, TONS;
-	}
-	
-	public enum Glow {
-		LITTLE, MEDIUM, LOTS;
-	}*/
-		
-/*				- Size (enum) - [Tiny, Small, Medium, Large, Huge]   // Factors into amount of epoxy base solution needed, general (batch quantities)
-				- Weight - How much item weighs overall   // Factors into amount of epoxy base solution needed, accurate (individual)
-				- Basic Effect Level (enum) - [Hardly Any [1], Little [2], Medium [3], Lot [4], Tons [5]]
-				- Advanced Effect Level (enum) - [Hardly Any [1], Little [2], Medium [3], Lot [4], Tons [5]]
-				- Glow Level (enum) - [Little [1], Medium [2], Lot [3]]
-				- Extras (Map (Object, Price)) - Static cost add-on determined using component as a key and price as a value
-	
-	}*/
-	
-	
+
 }
